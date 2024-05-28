ac3365f05a6fe8cd822ecba5784b8d10e59175e5
Jonathan González <135473821+safebytelabs-jgonf@users.noreply.github.com>
2024-05-28 16:19:00 +0200

    Create app.py

---
+++ src/mypkg/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# pylint: disable=W0102,E0712,C0103,R0903
+
+""" PROJECT NAME """
+
+__updated__ = "2016-03-27 00:00:00"
+
+# TODO: Put your messages here 
+
+# general imports
+# third-party imports
+# local imports
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
