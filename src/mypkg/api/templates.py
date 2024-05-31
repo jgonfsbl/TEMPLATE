7f38579eb82a6801eb6b87e3532f6caeba363fc8
Jonathan González <135473821+safebytelabs-jgonf@users.noreply.github.com>
2024-05-31 07:07:52 +0200

    Create templates.py

---
+++ src/mypkg/api/templates.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# pylint: disable=W0102,E0712,C0103,R0903
+
+""" MYPKG """
+
+__updated__ = "2024-05-12 12:19:36"
+
+
+def get_templates():
+    """Code to fetch templates"""
+    return "Templates details"
+
+
+def add_templates():
+    """Code to add a new template"""
+    return "Template added"
+
+
+def get_template_id(template_id: str):
+    """Code to get an existing template"""
+    return f"Template details for {template_id}"
+
+
+def update_template_id(template_id: str):
+    """Code to update an existing template"""
+    return f"Template updated: {template_id}"
+
+
+def delete_template_id(template_id: str):
+    """Code to delete an existing template"""
+    return f"Template deleted: {template_id}"
