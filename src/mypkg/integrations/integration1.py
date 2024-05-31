ea47c91bda83f03b60c31ad003977301bba2bd7d
Jonathan González <jgonf@safebytelabs.com>
2024-05-31 17:09:19 +0200

    boilerplate integration

---
+++ src/mypkg/integrations/integration1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# pylint: disable=W0102,E0712,C0103,R0903
+
+""" MYPKG """
+
+__updated__ = "2024-05-31 13:15:14"
+
+
+def integration1_message():
+    """Code to integrate SOSCom"""
+    return "Integration1 performed successfully"
