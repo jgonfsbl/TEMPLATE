02604a5e41686a549db17e8b2cd614b19c81a947
Jonathan González <135473821+safebytelabs-jgonf@users.noreply.github.com>
2024-05-31 07:07:19 +0200

    Create security.py

---
+++ src/mypkg/api/security.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# pylint: disable=W0102,E0712,C0103,R0903
+
+""" MYPKG """
+
+__updated__ = "2024-05-12 12:20:08"
+
+
+def authenticate():
+    """Code to authenticate a user"""
+
+
+def authorize():
+    """Code to authorize a user"""
